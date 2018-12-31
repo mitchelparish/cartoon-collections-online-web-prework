b9b41fbd93ee4b617661f99e3e4eadc5e5b11145
Mitchel Parish <mitchelparish@gmail.com>
2018-12-31 20:22:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves(dwarfnames)
-  puts dwarfnames.each_with_index
+  puts dwarfnames.each_with_index {|name, index|
+  puts "#{index + 1}. #{name}"}
+end
 end
 
 def summon_captain_planet# code an argument here
